805036d855475db4194c4e04ab472e862709aa0b
Ronny <ronnychamba96@gmail.com>
2022-07-15 23:09:21 -0500

    Jugador en linea-cerrar sesion

--- app/src/main/java/com/example/matapayasos/Menu.java
+++ app/src/main/java/com/example/matapayasos/Menu.java
@@ -2,13 +2,56 @@ package com.example.matapayasos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Menu extends AppCompatActivity {
+
+
+     FirebaseAuth auth;
+     FirebaseUser user;
+     Button btnCerrarSesion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        btnCerrarSesion = findViewById(R.id.btnCerrarSesion);
+
+        auth = FirebaseAuth.getInstance();
+        user = auth.getCurrentUser();
+
+        btnCerrarSesion.setOnClickListener( (event)-> cerrarSesion());
+
     }
+
+    @Override
+    protected void onStart() {
+        usuarioLogeado();
+        super.onStart();
+    }
+
+    private void usuarioLogeado() {
+
+        if (user != null) {
+            Toast.makeText(this, "en linea", Toast.LENGTH_LONG).show();
+        } else {
+            startActivity(new Intent(this, MainActivity.class));
+            finish();
+        }
+    }
+
+    private void cerrarSesion(){
+        auth.signOut();
+        startActivity(new Intent(Menu.this, MainActivity.class));
+        Toast.makeText(this, "Sesión cerrada exitosamente", Toast.LENGTH_SHORT).show();
+
+    }
+
 }
